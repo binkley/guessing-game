77f0fc7c4887ff660320fa8235c249a12acdbf04
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-03-11 15:52:33 -0500

    It runs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Guessing Game
+
+Exploring Rust.
+
+See https://doc.rust-lang.org/book/second-edition/ch02-00-guessing-game-tutorial.html.
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+extern crate rand;
+
+use std::io;
+use std::io::Write;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        print!("Please input your guess: ");
+        io::stdout().flush()
+            .expect("Failed to flush stdout");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number!");
+                continue;
+            },
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("Just right.");
+                break;
+            },
+        };
+    }
+}
